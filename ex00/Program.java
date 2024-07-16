314adc0134898001f65a56d35c5fdf4aec9219e2
Mohammed Marzouki <mohammed0marzouki@gmail.com>
2024-07-16 17:33:53 +0100

    479598

--- ex00/Program.java
+++ ex00/Program.java
@@ -1,8 +1,15 @@
-
 public class Program {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+      int input = 479598;
+      int modulos = 10;
+      int result = 0;
+
+      for (int i = 0 ; i < 6 ; i++){
+        int del = input % modulos;
+        input -= del;
+        result += del / (modulos / 10);
+        modulos *= 10;
+      }
+      System.out.println(result);
     }
-    
-}
+  } 
